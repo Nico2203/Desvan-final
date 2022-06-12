f03e2cb25d67c15a57178fa7cd71e27219270e25
Nico2203 <104037509+Nico2203@users.noreply.github.com>
2022-06-11 23:45:56 -0300

    Subida proyecto

---
+++ 404.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagina principal</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: sans-serif;
+        }
+        body{
+            backgraund: #yellow
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .container-404{
+            te
+        }
+    </style>
+</head>
+<body>
+    <div class="container-404">
+        <h1>Error 404</h1>
+        <p>Esta pagina no se encuentra dispinible en estos momentos. Disculpa las molestias.</p>
+        <a href="index.html"class="boton>
+        pagina principal
+    </a>
+
+    </div>
+
+</body>
+</html>
